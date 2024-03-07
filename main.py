1b937eb038651e32dcc9a1189410a8b63ab0f311
Pieter de Jong <pieter.a.dejong@gmail.com>
2024-03-07 16:10:56 -0500

    finished phase2 functionally, needs some cleanup and maintainability

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ Overall notes as I go:
 else, we'd have to perform DELETE calls to ensure all non-POLYANET/etc cells are set to SPACE.
 (This presumes the DELETE call does in fact set the value to SPACE.)
 
-Room for improvement:
-- reduce number of HTTP calls
-- 
 
 Phase 2:
 - fetch current map
@@ -23,6 +20,12 @@ Phase 2:
 - verify result at `https://challenge.crossmint.io/map`
 - approach: traversal + individual POSTs seems necessary, as the endpoints do not accept a list of cells to update;
 and there is no quickly discernable "algorithmic" way to determine calls to make.
+- cell values on my map seem to include:
+{'LEFT_COMETH', 'RED_SOLOON', 'BLUE_SOLOON', 'DOWN_COMETH', 'SPACE', 'POLYANET', 'PURPLE_SOLOON', 'RIGHT_COMETH', 'UP_COMETH', 'WHITE_SOLOON'}
+
+Room for improvement:
+- reduce number of HTTP calls
+- 
 
 """
 
@@ -40,6 +43,17 @@ class CelestialBody(Enum):
     SOLOON = 'SOLOON'
     COMET = 'COMET'
 
+    # Phase 2
+    RED_SOLOON = 'RED_SOLOON'
+    BLUE_SOLOON = 'BLUE_SOLOON'
+    PURPLE_SOLOON = 'PURPLE_SOLOON'
+    WHITE_SOLOON = 'WHITE_SOLOON'
+
+    UP_COMETH = 'UP_COMETH'
+    RIGHT_COMETH = 'RIGHT_COMETH'
+    DOWN_COMETH = 'DOWN_COMETH'
+    LEFT_COMETH = 'LEFT_COMETH'
+    
 def load_config():
     try:
         with open('config.json', 'r') as f:
@@ -58,7 +72,7 @@ def load_config():
         sys.exit(1)
     
 
-def add_polyanet(i,j) -> None:
+def add_polyanet(i, j, value = '') -> None:
     payload = {'row': i, 'column': j, 'candidateId': candidateId}
     response = requests.post(f'{BASE_API_URL}/polyanets', json=payload)
     if response.status_code == 200:
@@ -66,10 +80,50 @@ def add_polyanet(i,j) -> None:
     else:
         logging.error('Failed to add polyanet at row: %s, column: %s - Status Code: %s', r, c, response.status_code)
 
+def add_soloon(i, j, value) -> None:
+    # this function should make the POST req and know about the exact params based on color.
+    color = value.split('_')[0].lower()
+    payload = {'row': i, 'column': j, 'candidateId': candidateId, 'color': color} # TODO: text/value validation. now fragile
+    response = requests.post(f'{BASE_API_URL}/soloons', json=payload)
+    if response.status_code == 200:
+        logging.info('Successfully added soloon at row: %s, column: %s', i, j)
+    else:
+        logging.error('Failed to add soloon at row: %s, column: %s - Status Code: %s', i, j, response.status_code)
+
+def add_comet(i, j, value) -> None:
+    direction = value.split('_')[0].lower()
+    payload = {'row': i, 'column': j, 'candidateId': candidateId, 'direction': direction}
+    response = requests.post(f'{BASE_API_URL}/comeths', json=payload)
+    if response.status_code == 200:
+        logging.info('Successfully added comet at row: %s, column: %s', i, j)
+    else:
+        logging.error('Failed to add comet at row: %s, column: %s - Status Code: %s', i, j, response.status_code)
+
 def handle(i, j, cell):
     if cell == CelestialBody.POLYANET.value:
         print(f'Polyanet found at: {i} {j}')
         add_polyanet(i, j)
+
+def updateMap(i, j, cell):
+    # this function should only determine polyanet/comet/soloon, and call resp. funciton
+    # with the original value. strategy pattern
+    if 'SOLOON' in cell:
+        add_soloon(i, j, cell)
+    elif 'POLYANET' in cell:
+        add_polyanet(i, j, cell)
+    elif 'COMET' in cell:
+        add_comet(i, j, cell)
+    
+
+    # match cell:
+    #     case CelestialBody.POLYANET.value:
+    #         add_polyanet(i, j)
+    #     case CelestialBody.SOLOON.value:
+    #         add_soloon(i, j)
+    #     case CelestialBody.COMET.value:
+    #         add_comet(i, j)
+    #     case _:
+    #         logging.warning(f'Unknown celestial body {cell} at position ({i}, {j})')
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -88,7 +142,11 @@ def main():
     # Phase 2:
     resp = requests.get(f'{BASE_API_URL}/map/{candidateId}/goal')
     goal = resp.json().get('goal') 
-    print(goal)
+    options = set()
+    for i, row in enumerate(goal):
+        for j, cell in enumerate(row):
+            # options.add(cell)
+            updateMap(i, j, cell)
 
 
 if __name__ == '__main__':
